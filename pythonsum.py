3cf444670ab909b4fcdba7e1c9f59194cba6449d
Nilaksh1 <40692589+Nilaksh1@users.noreply.github.com>
2023-08-26 22:04:09 +0530

    Create pythonsum.py

---
+++ pythonsum.py
@@ -0,0 +1,17 @@
+# Python3 program to add two numbers
+
+def Addition():
+    global num1,num2
+    num1 = 15
+    num2 = 12
+
+    # Adding two nos
+    sum = num1 + num2
+    return sum
+
+
+# printing values
+sum=Addition()
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
+
+
